f45c72394db0c8f664af56b6854ceb569da42a13
grant65404 <grnatb05@gmail.com>
2025-10-31 15:47:31 -0600

    shidding

--- Task2Main.m
+++ Task2Main.m
@@ -2,11 +2,13 @@ close all; clear; clc;
 %% Part 1 Code 
 %% task 2 deliv 1
 alpha = 5; % angle of attack in degrees
-for i=1:100
-[xvar,y_t,y_c,dy_c,xi,x_U,x_L,y_U,y_L,Name] = AirfoilGeo(1,0,0,12,i*100,0);
-x = unique([x_U,x_L]);
-y = unique([y_U,y_L]);
-cl(i) = Vortex_Panel(x,y,alpha);
+for i=1:20
+    [xvar,y_t,y_c,dy_c,xi,x_U,x_L,y_U,y_L,Name] = AirfoilGeo(1,0,0,12,i*10,0);
+    x = unique([x_U,x_L],'stable');
+    y = unique([y_U,y_L],'stable');
+    cl(i) = Vortex_Panel(x,y,alpha);
+    n(i)=i;
 end
-figure
-plot(i,cl)
+figure()
+plot(x,y)
+plot(n,cl)
