b7916788c82592694114e57a53f859dfe7fa1f91
JoshSavageCU <joshua.savage@colorado.edu>
2025-11-07 14:34:31 -0700

    Add files via upload

---
+++ Part1_Task1.m
@@ -0,0 +1,47 @@
+clear
+clc
+close all
+tic
+
+%% Part 1, Task 1
+% NACA 0018
+c = 1;
+D1 = 0; % Must be 1 digit
+D2 = 0; % Must be 1 digit
+D3 = 18; % Must be 1 or 2 digits
+n = 500;
+
+[x,y_t,y_c,dy_c,xi,XB,YB,Name] = AirfoilGeo(c,D1,D2,D3,n);
+
+figure()
+hold on
+axis equal
+plot(x,y_c)
+plot(XB,YB,"k")
+title(Name)
+xlabel("Chord Length [m]")
+ylabel("Airfoil Thickness [m]")
+legend("Mean Camber Line","Airfoil Geometry","Location","northeast")
+
+% NACA 2418
+c = 1;
+D1 = 2; % Must be 1 digit
+D2 = 4; % Must be 1 digit
+D3 = 18; % Must be 1 or 2 digits
+n = 500;
+
+[x,y_t,y_c,dy_c,xi,XB,YB,Name] = AirfoilGeo(c,D1,D2,D3,n);
+
+figure()
+hold on
+axis equal
+plot(x,y_c)
+plot(XB,YB,"k")
+title(Name)
+xlabel("Chord Length [m]")
+ylabel("Airfoil Thickness [m]")
+legend("Mean Camber Line","Airfoil Geometry","Location","northeast")
+
+clear D1 D2 D3 n x y_t y_c xi XB YB Name
+
+runtime = toc;
