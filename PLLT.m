7931a507a9de86f169ef10a6514f4e305ecff6dc
JoshSavageCU <joshua.savage@colorado.edu>
2025-11-18 19:12:16 -0700

    Add files via upload

--- PLLT.m
+++ PLLT.m
@@ -19,17 +19,17 @@ AR = (b^2)/(0.5*b*(c_r+c_t));
 
     x = D/M;
 
-    for i = 1:N
-        A(2*i - 1) = x(i);
-        A(2*i) = 0;
-    end
+    % for i = 1:N           % didn't work for whatever reason
+    %     A(2*i - 1) = x(i);
+    %     A(2*i) = 0;
+    % end
 
     Delta = 0;
     for n = 2:N
-        Delta = Delta + n*(A(n)/A(1))^2;
+        Delta = Delta + (2*n - 1)*(x(n)/x(1))^2;
     end
 
-    c_L = A(1)*pi()*AR;
+    c_L = x(1)*pi()*AR;
     c_Di = ((c_L^2)/(pi()*AR))*(1+Delta);
     e = 1/(1+Delta);
 
